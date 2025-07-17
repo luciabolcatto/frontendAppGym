8bea613c0316dd219f6168d5f61a0054392005ba
Lucia Bolcatto <lucibolcatto11@gmail.com>
2025-07-17 15:57:44 -0300

    pages home, login, and register components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Login from './pages/login';
+import Register from './pages/Register';
 
 function App() {
   const [count, setCount] = useState(0)
