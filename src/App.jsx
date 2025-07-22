e1f278bb84133d198d83c06f3d9fd439237eca41
Lucia Bolcatto <lucibolcatto11@gmail.com>
2025-07-21 21:36:06 -0300

    funciona

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-import Landing from './pages/Landing';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import Landing from './pages/landing';
+import Login from './pages/login';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
